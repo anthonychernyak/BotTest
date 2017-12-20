b8b274e36fe18acc572675e269e2bdfbbc92bedc
Anthony Chernyak <anthonychernyak@Anthony-Chernyak.local>
2017-12-20 14:44:43 -0500

    Refactoring

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 import requests
 import threading
 import json
-import datetime
+from datetime import datetime
+import values
 
 migrations = []
-auth = ('ant6190', 'temp')
+auth = ('ant6190', values.key)
+GIT_URL = 'https://api.github.com/repos'
 
 COMMENT_INFO = {
-  "body": "Hey, your Migration is out of date! You should fix that.\n Fix it with Tyler's Script: https://gist.github.com/CNBorn/7c312744b34c9b6e9b853382fe93385f\nThe last migration to integration happened at {0}",
+  "body": "Hey, your Migration is out of date! You should fix that.\n "
+          "Fix it with Tyler's Script: "
+          "https://gist.github.com/CNBorn/7c312744b34c9b6e9b853382fe93385f\n"
+          "The last migration to integration happened at {0}",
   "event": "COMMENT",
 }
 
@@ -15,8 +20,8 @@ COMMENT_INFO = {
 def check_review_status(migration, time):
     COMMENT_INFO['body'] = COMMENT_INFO['body'].format(time)
     requests.post(
-        'https://api.github.com/repos/ant6190/BotTest/pulls/{0}/reviews'.format(
-            migration), auth=auth, data=json.dumps(COMMENT_INFO))
+        '{0}/ant6190/BotTest/pulls/{1}/reviews'.format(GIT_URL, migration),
+        auth=auth, data=json.dumps(COMMENT_INFO))
 
 
 def send_comments(merged_migrations):
@@ -25,17 +30,20 @@ def send_comments(merged_migrations):
     for merged_migration in merged_migrations:
         migrations.remove(merged_migration)
         for migration in migrations:
-            check_review_status(migration, str(datetime.datetime.now().time()))
+            check_review_status(migration, str(datetime.now().time()))
 
 
 def main():
     threading.Timer(5.0, main).start()
-    r = requests.get('https://api.github.com/repos/ant6190/BotTest/pulls', auth=auth)
+    r = requests.get('{0}/ant6190/BotTest/pulls'.format(GIT_URL),
+                     auth=auth)
     if r.json():
-        for item in r.json():
-            pr = requests.get(item['url'], auth=auth).json()
-            if '[Migration]' in pr['title'] and pr['number'] not in migrations and pr['state'] == 'open':
+        for pr in r.json():
+            if '[Migration]' in pr['title'] \
+                    and pr['number'] not in migrations \
+                    and pr['state'] == 'open':
                 migrations.append(pr['number'])
+
     else:
         print("No PRs to Check")
 
@@ -43,7 +51,9 @@ def main():
 
     merged_migrations = []
     for migration in migrations:
-        request = requests.get('https://api.github.com/repos/ant6190/BotTest/pulls/{0}/merge'.format(migration), auth=auth)
+        request = requests.get(
+            '{0}/ant6190/BotTest/pulls/{1}/merge'.format(GIT_URL, migration),
+            auth=auth)
         if request.status_code == 204:
             merged_migrations.append(migration)
     if merged_migrations:
