fe0080b29321fb4bb97a040ea89fc4210808764c
Anthony Chernyak <anthonychernyak@Anthony-Chernyak.local>
2017-12-20 10:34:53 -0500

    Last Change

--- bot.py
+++ bot.py
@@ -1,11 +1,28 @@
 import requests
 import threading
 
+migrations = []
+auth = ('ant6190', 'temp')
+
+COMMENT_INFO = {
+  "body": "Hey, your Migration is out of date! You should fix that",
+  "event": "REQUEST_CHANGES",
+}
+
+
+def send_comments(merged_migrations):
+    print('Merged Migrations')
+    print(merged_migrations)
+    for merged_migration in merged_migrations:
+        migrations.remove(merged_migration)
+        for migration in migrations:
+            requests.post(
+                'https://api.github.com/repos/ant6190/BotTest/pulls/{0}/reviews'.format(
+                    migration), auth=auth, data=COMMENT_INFO)
+
 
 def main():
     threading.Timer(5.0, main).start()
-    migrations = []
-    auth = ('ant6190', 'temp')
     r = requests.get('https://api.github.com/repos/ant6190/BotTest/pulls', auth=auth)
     if r.json():
         for item in r.json():
@@ -17,9 +34,14 @@ def main():
 
     print(migrations)
 
+    merged_migrations = []
     for migration in migrations:
-        request = requests.get('https://api.github.com/repos/ant6190/pulls/{0}/merge'.format(migration), auth=auth)
-        print(request.status_code)
+        request = requests.get('https://api.github.com/repos/ant6190/BotTest/pulls/{0}/merge'.format(migration), auth=auth)
+        if request.status_code == 204:
+            merged_migrations.append(migration)
+    if merged_migrations:
+        send_comments(merged_migrations)
+
 
 main()
 
